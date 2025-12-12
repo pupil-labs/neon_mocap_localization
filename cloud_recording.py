378a7a45903e2b38b4b2c4bbf454e4f18234570e
Robert Ennis <rob@pupil-labs.com>
2025-12-12 17:56:59 +0100

    support cloud recordings

---
+++ cloud_recording.py
@@ -0,0 +1,89 @@
+import json
+from pathlib import Path
+
+import numpy as np
+import av
+import cv2
+import pandas as pd
+
+
+class Scene:
+    def __init__(self, rec_path):
+        self.rec_path = rec_path
+
+        self.time_csv = pd.read_csv(self.rec_path / "world_timestamps.csv")
+
+        self.idx = self.time_csv.index.to_numpy()
+        self.time = self.time_csv["timestamp [ns]"].to_numpy()
+
+        self.nframes = len(self.time_csv)
+
+        scene_mp4_path = [f for f in self.rec_path.glob("*.mp4")][0]
+
+        self.container = av.open(scene_mp4_path)
+        self.video_stream = self.container.streams.video[0]
+
+        self.duration = self.video_stream.duration
+
+    def _seek(self, vid_time):
+        self.container.seek(
+            int(vid_time / self.video_stream.time_base), stream=self.video_stream
+        )
+
+    def _get_img(self, vid_time):
+        exact_frame = None
+        for frame in self.container.decode(self.video_stream):
+            # find the first frame >= the requested timestamp
+            if frame.time >= vid_time:
+                exact_frame = frame
+                break
+
+        if exact_frame is not None:
+            return exact_frame.to_ndarray(format="rgb24")
+        else:
+            return None
+
+    def bgr_at_time(self, time):
+        vid_time = (time - self.time[0]) * 1e-9
+        self._seek(vid_time)
+        return cv2.cvtColor(self._get_img(vid_time), cv2.COLOR_RGB2BGR)
+
+
+class Gaze:
+    def __init__(self, rec_path):
+        self.rec_path = rec_path
+
+        self.data_csv = pd.read_csv(rec_path / "gaze.csv")
+        self.time = self.data["timestamp [ns]"].to_numpy()
+
+        self.nframes = len(self.data)
+
+        self.data = {}
+        self.data["point_x"] = self.data_csv["gaze x [px]"]
+        self.data["point_y"] = self.data_csv["gaze y [px]"]
+
+
+class Calibration:
+    def __init__(self, rec_path):
+        self.calib_path = rec_path / "scene_camera.json"
+
+        scene_calib = []
+        with open(self.calib_path, "r") as f:
+            scene_calib = json.load(f)
+
+        self.scene_camera_matrix = np.array(scene_calib["camera_matrix"])
+        self.scene_distortion_coefficients = np.array(
+            scene_calib["distortion_coefficients"]
+        )
+
+
+class CloudRecording:
+    def __init__(self, directory):
+        self.directory = Path(directory)
+
+        self.info = []
+        with open(self.directory / "info.json", "r") as f:
+            self.info = json.load(f)
+
+        self.scene = Scene(self.directory)
+        self.calibration = Calibration(self.directory)
